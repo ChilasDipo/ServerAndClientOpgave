6d71832ea96e19f634fe976ba5daeeda98273864
ChilasDipo <Chil0041@edu.easj.dk>
2021-09-17 10:54:55 +0200

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,62 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    PrintWriter toServer = null;
+    Scanner fromServer = null;
+    Scanner scanner = new Scanner(System.in);
+
+    void client(){
+        try{
+        Socket socket = new Socket("localhost",9000);
+
+        fromServer = new Scanner(socket.getInputStream());
+        toServer = new PrintWriter(socket.getOutputStream(),true);
+        System.out.println("Connection Done");
+        new Thread(() -> {
+           while (true){
+               if (fromServer.hasNext()){
+                   System.out.println(fromServer.nextLine());
+               }
+           }
+
+        }).start();
+
+
+        while (true){
+
+            toServer.println(scanner.nextLine());
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+        }catch(IOException e){
+            System.err.println(e);
+        }
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.client();
+    }
+
+}
